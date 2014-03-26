9e26bdefbce2d8c6ce3fd7b7f32bd34ae6347d43
Spike Frye <sebfrye@gmail.com>
2014-03-25 21:15:34 -0500

    Work so far.

---
+++ api/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	publish_date = models.DateTimeField()
+
+    class Meta:
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
+
+    def __unicode__(self):
+    	return u'%s' % self.title
+
